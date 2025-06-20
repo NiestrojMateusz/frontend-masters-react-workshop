c28ba208ea79ce28613d01d7057b66b20a19970d
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2025-06-20 20:47:38 +0200

    Solve exercise 2 - use xstate context

--- src/02/timerMachine.js
+++ src/02/timerMachine.js
@@ -4,12 +4,20 @@ export const timerMachine = createMachine({
   initial: 'idle',
   // Add initial context
   // ...
-
+  context: {
+    duration: 60,
+    elapsed: 0,
+    interval: 0.1,
+  },
   states: {
     idle: {
       // Reset duration and elapsed on entry
       // ...
-
+      entry: assign({
+        duration: 60,
+        elapsed: 0,
+        interval: 0.1,
+      }),
       on: {
         TOGGLE: 'running',
       },
@@ -17,9 +25,11 @@ export const timerMachine = createMachine({
     running: {
       on: {
         TOGGLE: 'paused',
-
-        // On ADD_MINUTE, increment context.duration by 60 seconds
-        // ...
+        ADD_MINUTE: {
+          actions: assign({
+            duration: (context) => context.duration + 60,
+          }),
+        },
       },
     },
     paused: {
